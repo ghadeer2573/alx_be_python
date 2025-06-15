3e392c33e87e45ba8a8b1794ad1e72e71c91e24e
Ghadeer <ghadeer.raafat@gmail.com>
2025-06-15 20:50:25 +0300

    bank

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+            self.account_balance = initial_balance
+    def deposit(self,amount):
+          self.account_balance += amount
+          print(f"Deposited:${amount}")
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            print(f"Withdrew: ${amount}")
+            return True
+        else:
+            print("Insufficient funds. Withdrawal failed.")
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
