674f9fe6df80a0463c0659649e8b3475f0473ac5
Ghadeer <ghadeer.raafat@gmail.com>
2025-06-15 20:58:00 +0300

    bank alter

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,11 +3,11 @@ class BankAccount:
             self.account_balance = initial_balance
     def deposit(self,amount):
           self.account_balance += amount
-          print(f"Deposited:${amount}")
+          print(f"Deposited: ${amount:.1f}")
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount}")
+            print(f"Withdrew: ${amount:.1f}")
             return True
         else:
             print("Insufficient funds. Withdrawal failed.")
