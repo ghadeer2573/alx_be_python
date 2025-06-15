306c0ce808c4cb430d9f598e637b336c8fa2c93a
Ghadeer <ghadeer.raafat@gmail.com>
2025-06-15 21:02:57 +0300

    bank alter123

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,16 +2,21 @@ class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
 
+   # bank_account.py
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount:.1f}")
 
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount:.1f}")
+            return True
         else:
-            print("Insufficient funds.")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
